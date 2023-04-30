4a24d1004495963fb5b8c59240b18db6f76eaa05
Aleksandr <majoryman90@gmail.com>
2023-04-30 11:00:09 +0300

    homework9

---
+++ data.js
@@ -0,0 +1,55 @@
+const newApi =
+    `
+[
+  {
+    "API": "AdoptAPet",
+    "Description": "Resource to help get pets adopted",
+    "Auth": "apiKey",
+    "HTTPS": true,
+    "Cors": "yes",
+    "Link": "https://www.adoptapet.com/public/apis/pet_list.html",
+    "Category": "Animals",
+    "img": "img/photo1.jpg"
+  },
+  {
+    "API": "Axolotl",
+    "Description": "Collection of axolotl pictures and facts",
+    "Auth": "",
+    "HTTPS": true,
+    "Cors": "no",
+    "Link": "https://theaxolotlapi.netlify.app/",
+    "Category": "Animals",
+    "img": "img/photo2.jpg"
+  },
+  {
+    "API": "Cat Facts",
+    "Description": "Daily cat facts",
+    "Auth": "",
+    "HTTPS": true,
+    "Cors": "no",
+    "Link": "https://alexwohlbruck.github.io/cat-facts/",
+    "Category": "Animals",
+    "img": "img/photo3.jpg"
+  },
+  {
+    "API": "Cataas",
+    "Description": "Cat as a service (cats pictures and gifs)",
+    "Auth": "",
+    "HTTPS": true,
+    "Cors": "no",
+    "Link": "https://cataas.com/",
+    "Category": "Animals",
+    "img": "img/photo4.jpg"
+  },
+  {
+    "API": "Cats",
+    "Description": "Pictures of cats from Tumblr",
+    "Auth": "apiKey",
+    "HTTPS": true,
+    "Cors": "no",
+    "Link": "https://docs.thecatapi.com/",
+    "Category": "Animals",
+    "img": "img/photo5.jpg"
+  }
+]
+`
