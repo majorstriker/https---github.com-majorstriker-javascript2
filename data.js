2a3de4c756f71ee085917081d5bbe67211f57065
Aleksandr <majoryman90@gmail.com>
2023-05-08 11:59:11 +0300

    homework10

--- data.js
+++ data.js
@@ -1,55 +1,41 @@
-const newApi =
+const items =
     `
 [
   {
-    "API": "AdoptAPet",
-    "Description": "Resource to help get pets adopted",
-    "Auth": "apiKey",
-    "HTTPS": true,
-    "Cors": "yes",
-    "Link": "https://www.adoptapet.com/public/apis/pet_list.html",
-    "Category": "Animals",
-    "img": "img/photo1.jpg"
+    "img": "img/photo1.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": 52.00
   },
   {
-    "API": "Axolotl",
-    "Description": "Collection of axolotl pictures and facts",
-    "Auth": "",
-    "HTTPS": true,
-    "Cors": "no",
-    "Link": "https://theaxolotlapi.netlify.app/",
-    "Category": "Animals",
-    "img": "img/photo2.jpg"
+    "img": "img/photo2.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": 52.00
   },
   {
-    "API": "Cat Facts",
-    "Description": "Daily cat facts",
-    "Auth": "",
-    "HTTPS": true,
-    "Cors": "no",
-    "Link": "https://alexwohlbruck.github.io/cat-facts/",
-    "Category": "Animals",
-    "img": "img/photo3.jpg"
+    "img": "img/photo3.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": 52.00
   },
   {
-    "API": "Cataas",
-    "Description": "Cat as a service (cats pictures and gifs)",
-    "Auth": "",
-    "HTTPS": true,
-    "Cors": "no",
-    "Link": "https://cataas.com/",
-    "Category": "Animals",
-    "img": "img/photo4.jpg"
+    "img": "img/photo4.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": 52.00
   },
   {
-    "API": "Cats",
-    "Description": "Pictures of cats from Tumblr",
-    "Auth": "apiKey",
-    "HTTPS": true,
-    "Cors": "no",
-    "Link": "https://docs.thecatapi.com/",
-    "Category": "Animals",
-    "img": "img/photo5.jpg"
+    "img": "img/photo5.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": 52.00
+  },
+  {
+    "img": "img/photo6.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": 52.00
   }
 ]
 `
