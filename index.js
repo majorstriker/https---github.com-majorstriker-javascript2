569e92d619b68d849945500c7941de1b1ce5d9b2
Aleksandr <majoryman90@gmail.com>
2023-05-27 14:18:13 +0300

    final

--- index.js
+++ index.js
@@ -67,18 +67,97 @@ productsData.forEach(productData => {
 });
 
 
+const cartDivEl = document.querySelector('.cart__items__div');
+const productAdd = document.querySelectorAll('.product__add');
+const cartBlockLeftEl = document.querySelector('.cart__block__left');
 
-const advantagesEl = document.querySelector('.advantages');
-const productAdd = document.querySelector('.product__add');
+for (const item of productAdd) {
+    item.addEventListener('click', function() {
+        const cartBlockLeftCartEl = document.createElement('div');
+        cartBlockLeftCartEl.classList.add('cart__block__left__card');
 
-productAdd.addEventListener('click', function() {
-    const cartDivEl = document.createElement('div');
-    cartDivEl.classList.add('cart__items__div');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('alt', 'cart_1');
+        productImgCart.src = './img/product_cart_1.jpg';
 
-    const cardHeaderEl = document.createElement('h1');
-    cardHeaderEl.classList.add('cart__items__div__header');
-    cardHeaderEl.textContent = 'Cart Items';
+        const cartBlockLeftCardInfo = document.createElement('div');
+        cartBlockLeftCardInfo.classList.add('cart__block__left__card__info');
 
-    advantagesEl.after(cartDivEl);
-    cartDivEl.append(cardHeaderEl);
+        const cartBlockLeftCardInfoLinks = document.createElement('div');
+        cartBlockLeftCardInfoLinks.classList.add('cart__block__left__card__info__links');
+
+        const cartBlockLeftCardInfoLinksA1 = document.createElement('a');
+        cartBlockLeftCardInfoLinksA1.classList.add('cart__block__left__card__info__a');
+        cartBlockLeftCardInfoLinksA1.innerHTML = 'MANGO PEOPLE <br>';
+
+        const cartBlockLeftCardInfoLinksA2 = document.createElement('a');
+        cartBlockLeftCardInfoLinksA2.classList.add('cart__block__left__card__info__a');
+        cartBlockLeftCardInfoLinksA2.textContent = 'T-SHIRT';
+
+        const cartBlockLeftCardInfoText = document.createElement('div');
+        cartBlockLeftCardInfoText.classList.add('cart__block__left__card__info__text');
+
+        const cartBlockLeftCardInfoTextP1 = document.createElement('p');
+        cartBlockLeftCardInfoTextP1.textContent = 'Price: ';
+        const cartBlockLeftCardInfoTextPrice = document.createElement('span');
+        cartBlockLeftCardInfoTextPrice.classList.add('cart__block__left__card__info__text__price');
+        cartBlockLeftCardInfoTextPrice.textContent = '$300';
+
+        const cartBlockLeftCardInfoTextP2 = document.createElement('p');
+        cartBlockLeftCardInfoTextP2.textContent = 'Color: Red';
+
+        const cartBlockLeftCardInfoTextP3 = document.createElement('p');
+        cartBlockLeftCardInfoTextP3.textContent = 'Size: Xl';
+
+        const cartBlockLeftCardInfoQuantity = document.createElement('div');
+        cartBlockLeftCardInfoQuantity.classList.add('cart__block__left__card__info__quantity');
+
+        const cartBlockLeftCardInfoQuantityP = document.createElement('p');
+        cartBlockLeftCardInfoQuantityP.textContent = 'Quantity:';
+
+        const cartblockLeftCardInfoQuantityInput = document.createElement('input');
+        cartblockLeftCardInfoQuantityInput.classList.add('cart__block__left__card__info__quantity__input');
+        cartblockLeftCardInfoQuantityInput.type = 'number';
+        cartblockLeftCardInfoQuantityInput.placeholder = '2';
+
+        const cartBlockClose = document.createElement('a');
+        cartBlockClose.classList.add('cart__block_close');
+
+        const cartBlockCloseIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        cartBlockCloseIcon.setAttribute('width', '18');
+        cartBlockCloseIcon.setAttribute('height', '18');
+        cartBlockCloseIcon.setAttribute('viewBox', '0 0 18 18');
+        cartBlockCloseIcon.setAttribute('fill', 'none');
+        cartBlockCloseIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+        const cartBlockCloseIconpathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        cartBlockCloseIconpathElement.setAttribute('d', 'M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467833C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z');
+        cartBlockCloseIconpathElement.setAttribute('fill', '#575757');
+
+        cartBlockLeftEl.append(cartBlockLeftCartEl);
+        cartBlockLeftCartEl.append(productImgCart);
+        cartBlockLeftCartEl.append(cartBlockLeftCardInfo);
+        cartBlockLeftCardInfo.append(cartBlockLeftCardInfoLinks);
+        cartBlockLeftCardInfoLinks.append(cartBlockLeftCardInfoLinksA1);
+        cartBlockLeftCardInfoLinks.append(cartBlockLeftCardInfoLinksA2);
+        cartBlockLeftCardInfo.append(cartBlockLeftCardInfoText);
+        cartBlockLeftCardInfoText.append(cartBlockLeftCardInfoTextP1);
+        cartBlockLeftCardInfoTextP1.append(cartBlockLeftCardInfoTextPrice);
+        cartBlockLeftCardInfoText.append(cartBlockLeftCardInfoTextP2);
+        cartBlockLeftCardInfoText.append(cartBlockLeftCardInfoTextP3);
+        cartBlockLeftCardInfoText.append(cartBlockLeftCardInfoQuantity);
+        cartBlockLeftCardInfoQuantity.append(cartBlockLeftCardInfoQuantityP);
+        cartBlockLeftCardInfoQuantity.append(cartblockLeftCardInfoQuantityInput);
+        cartBlockLeftCartEl.append(cartBlockClose);
+        cartBlockClose.append(cartBlockCloseIcon);
+        cartBlockCloseIcon.append(cartBlockCloseIconpathElement);
+
+        cartBlockClose.addEventListener('click', function() {
+            cartBlockLeftCartEl.remove();
+        });
+    });
+}
+
+cartBlockLeftEl.addEventListener('DOMNodeInserted', function() {
+    cartDivEl.style.display = 'block';
 });
